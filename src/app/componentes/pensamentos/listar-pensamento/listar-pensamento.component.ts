da905a24b20caf0ee960d61ee6c86ee5d65a0362
Mauro Cesar Martins Rosa <50694200115@serpro.gov.br>
2023-05-20 15:01:23 -0300

    busca interativa

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,18 +12,18 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = []; 
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
-  filtro: string = ""
+  filtro: string = ''
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMaisPensamentos(){
-    this.service.listar(++this.paginaAtual)
+    this.service.listar(++this.paginaAtual, this.filtro)
         .subscribe(listaPensamentos =>{
           this.listaPensamentos.push(...listaPensamentos);
           if(!listaPensamentos.length){
@@ -31,5 +31,14 @@ export class ListarPensamentoComponent implements OnInit {
           }
         })
   }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro)
+        .subscribe(listaPensamentos => {
+          this.listaPensamentos = listaPensamentos
+        })
+    }
 
 }
